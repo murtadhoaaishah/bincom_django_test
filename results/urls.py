a7dc216ad5fd0b200255e09b1787390c8ee1df9c
murtadhoaaishah <murtadhoaishat@gmail.com>
2024-10-05 15:57:13 +0100

    views updated

--- results/urls.py
+++ results/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('total-votes/', views.calculate_total_votes, name='total-votes'),
 ]
--- results/views.py
+++ results/views.py
@@ -12,3 +12,15 @@ def calculate_total_votes(request):
     }
     
     return render(request, 'results/total_votes.html', context)
+
+from django.shortcuts import render
+
+def index(request):
+    # Calculate total votes
+    total_votes = AnnouncedLGAResult.objects.aggregate(total=Sum('party_score'))['total'] or 0  # Use 0 if no votes
+    
+    context = {
+        'total_votes': total_votes
+    }
+    
+    return render(request, 'results/index.html', context)
